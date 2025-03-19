43f92de34b4670a09c47c0cb1ff7d096d480afae
chakihwan <rlghks0720@gmail.com>
2025-03-19 11:44:20 +0900

    Chp3 도메인, 퍼시스턴스, 서비스, 컨트롤러 작성

---
+++ src/main/java/kr/ac/kopo/kihwan/bookmarket/service/BookService.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.kihwan.bookmarket.service;
+
+import kr.ac.kopo.kihwan.bookmarket.domain.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> getAllBookList();
+}
