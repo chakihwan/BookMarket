13f22ce683f48df654b4c23e0c331492cd467c5f
chakihwan <rlghks0720@gmail.com>
2025-04-30 10:39:01 +0900

    5장 출판사명, 카테고리명 필터링 연습

--- src/main/java/kr/ac/kopo/kihwan/bookmarket/repository/BookRepositoryImpl.java
+++ src/main/java/kr/ac/kopo/kihwan/bookmarket/repository/BookRepositoryImpl.java
@@ -4,8 +4,7 @@ import kr.ac.kopo.kihwan.bookmarket.domain.Book;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
@@ -21,7 +20,7 @@ public class BookRepositoryImpl implements BookRepository {
                 " 이 책에서는 스프링 부트의 기본 개념을 쉽게 이해하고 다양한 실습 예제로 빠르게 익힐 수 있습니다.");
 //                + " 그리고 단계별 실습을 따라 하다 보면 도서 쇼핑몰 구축 프로젝트를 완성할 수 있습니다."+
 //                " 개념-실습-프로젝트의 3단계 학습으로 스프링 부트를 제대로 익힌다면 개발 시간을 단축하고 생산성을 높일 수 있는 개발자로 성장할 수 있습니다.");
-        book1.setPublisher("길벗캠퍼스");
+        book1.setPublisher("길벗");
         book1.setCategory("IT교재");
         book1.setUnitInStock(1000);
         book1.setReleaseDate("2024/12/31");
@@ -38,7 +37,7 @@ public class BookRepositoryImpl implements BookRepository {
 //                + " 자바스크립트의 기초부터 심화까지 핵심 문법을 학습한 후 12가지 프로그램을 만들며 학습한 내용을 확인할 수 있습니다."+
 //                " 문법 학습과 실습이 적절히 섞여 있어 프로그램을 만드는 방법을 재미있게 익힐 수 있고, 이 과정에서 프로그램의 작동 원리를 자연스럽게 이해할 수 있습니다.");
         book2.setPublisher("길벗");
-        book2.setCategory("IT교재");
+        book2.setCategory("IT전문서");
         book2.setUnitInStock(1000);
         book2.setReleaseDate("2024/02/20");
         book2.setCondition("신규도서");
@@ -55,7 +54,7 @@ public class BookRepositoryImpl implements BookRepository {
 //                "기 때문에 프로그래밍 언어 가운데 가장 쉽고 강력한 파이썬의 기초 내용부터 변수, 반복문,\n" +
 //                "자료구조, 객체 지향 프로그램의 탄탄한 활용까지 장별로 강화된 연습문제와 실무 프로젝트\n" +
 //                "등 규모 있는 프로그램으로 실력을 향상시킬 수 있습니다.");
-        book3.setPublisher("길벗");
+        book3.setPublisher("한빛");
         book3.setCategory("IT전문서");
         book3.setUnitInStock(1000);
         book3.setReleaseDate("2023/01/10");
@@ -96,5 +95,33 @@ public class BookRepositoryImpl implements BookRepository {
         }
         return booksByCategory;
     }
-//    147page
+    //    147page
+
+    @Override
+    public Set<Book> getBookListByFilter(Map<String, List<String>> filter) {
+        Set<Book> booksByPublisher = new HashSet<Book>();
+        Set<Book> booksByCategory = new HashSet<Book>();
+        Set<String> booksByFilter = filter.keySet();
+
+        if (booksByFilter.contains("publisher")) {
+            for (int i = 0; i < filter.get("publisher").size(); i++) {
+                String publisherName = filter.get("publisher").get(i);
+                for (Book book : listOfBooks) {
+                    if (publisherName.equalsIgnoreCase(book.getPublisher())) {
+                        booksByPublisher.add(book);
+                    }
+                    }
+                }
+            }
+        if (booksByFilter.contains("category")) {
+            for(int i = 0; i < filter.get("category").size(); i++) {
+                String categoryName = filter.get("category").get(i);
+                List<Book> list = getBookListByCategory(categoryName);
+                booksByCategory.addAll(list);
+            }
+        }
+//         저장된 요소 중에서 2 Set을 비교하여 같은 값만 남기고 나머지는 제거하는 역할(교집합만 남김)
+        booksByCategory.retainAll(booksByPublisher);
+        return booksByCategory;
+    }
 }
