76b54e9f28ca5f34d6d4bae6ffa572351fd696c7
chakihwan <rlghks0720@gmail.com>
2025-06-04 11:42:57 +0900

    BookMarket의 11장 예외처리 적용(카테고리 오류 상세표시 추가)

--- src/main/java/kr/ac/kopo/kihwan/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/kihwan/bookmarket/controller/BookController.java
@@ -70,7 +70,7 @@ public class BookController {
             "category") String category, Model model) {
         List<Book> booksByCategory = bookService.getBookListByCategory(category);
         if(booksByCategory == null || booksByCategory.isEmpty()) {
-            throw new CategoryException();
+            throw new CategoryException(category);
         }
         model.addAttribute("bookList", booksByCategory);
         return "books";
